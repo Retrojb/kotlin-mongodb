6ba2161c873fd0c062d59c3c3739295cdca1f3d5
Retrojb <baltescartography@gmail.com>
2019-08-03 14:38:06 -0400

    running ktor project connected with mongo cluster

--- src/main/kotlin/com/lmshub/lifestyle/lmsApplication.kt
+++ src/main/kotlin/com/lmshub/lifestyle/lmsApplication.kt
@@ -9,9 +9,10 @@ import com.lmshub.lifestyle.data.MongoDriver
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.StatusPages
+import io.ktor.features.*
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
@@ -22,8 +23,6 @@ import io.ktor.response.respondText
 import io.ktor.routing.*
 import org.bson.types.ObjectId
 
-fun main(args: Array<String>): Unit =
-        io.ktor.server.netty.EngineMain.main(args)
 //Local Environment
 const val host = "127.0.0.1"
 const val port = 27017
@@ -32,7 +31,7 @@ const val defaultDb = "lms-users-playground"
 const val devDb: String = "lms-dev-db"
 
 //Dev Environment
-const val devHost = " "
+const val devHost = ""
 const val devPort = 27029
 const val devUser = "lmsdbadm"
 val devUserPass = "lmsadmpass".toCharArray()
@@ -50,26 +49,63 @@ private val mongoDevDataService = MongoDriver (
         "lms-dev-db"
         )
 
-
+fun main(args: Array<String>) =
+        io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused")
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
+    install(ConditionalHeaders)
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(DataConversion)
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+// added testing: Boolean to false in the Application Mod args
+//    if (!testing) {
+//        install(HttpsRedirect) {
+//            // The port to redirect to. By default 443, the default HTTPS port.
+//            sslPort = 443
+//            // 301 Moved Permanently, or 302 Found redirect.
+//            permanentRedirect = true
+//        }
+//    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+//    val client = HttpClient(Jetty) {
+//    }
     //add this routing for static files later
     routing {
-        static("tbd") {
-            resources("tbd")
+        static("/static") {
+            resources("static")
         }
         install(StatusPages) {
             exception<AuthenticationException> { cause ->
                 call.respond(HttpStatusCode.Unauthorized)
             }
         }
-        route("/Users/johnbaltes/Desktop/mongodb") {
+        route("/data/db") {
             get {
                 call.respond(
                         mongoDataService.allFromCollection("col")
